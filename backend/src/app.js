a848e8cd19bd9d7f19a3971a0cfd45faf3378346
rohit97315 <rohitlamba97315@gmail.com>
2026-07-13 18:10:17 +0530

    done some changes for login

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials:true
 }))
 
